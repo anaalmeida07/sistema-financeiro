fb8f09ee9cd996fddf3314c7837183e526c942ec
Luiz H. Menezes <79934587+LuizHenriqueMenezes@users.noreply.github.com>
2024-05-15 21:54:46 -0300

    modal
    Co-Authored-By: Ana Carla  <anaalmeida.contato07@gmail.com>
    Co-Authored-By: Beatriz02Feitosa <109310646+Beatriz02Feitosa@users.noreply.github.com>

--- neneko/home.php
+++ neneko/home.php
@@ -16,6 +16,48 @@
             padding: 10px;
             margin-bottom: 10px;
         }
+
+        .modal {
+            display: none;
+            position: fixed;
+            z-index: 1;
+            padding-top: 100px;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            background-color: rgb(0, 0, 0);
+            background-color: rgba(0, 0, 0, 0.4);
+        }
+
+        .modal-content {
+            background-color: #fefefe;
+            margin: auto;
+            padding: 20px;
+            border: 1px solid #888;
+            width: 80%;
+        }
+
+        .close {
+            color: #aaa;
+            float: right;
+            font-size: 28px;
+            font-weight: bold;
+        }
+
+        .close:hover,
+        .close:focus {
+            color: #7952b3;
+            text-decoration: none;
+            cursor: pointer;
+        }
+
+        #addContaBtn, #addReceitaBtn, #addDespesaBtn{
+            background-color: #7952b3;
+            padding: 20px 15px;
+            margin: 10px;
+        }
     </style>
 </head>
 
@@ -30,100 +72,104 @@
             </ul>
         </nav>
     </div>
-    <h1 class="bv-home">Bem-vindo!</h1>
 
-    <div class="form-container">
-        <h2>Adicionar Nova Conta Bancária</h2>
-        <!-- Formulário para adicionar conta bancária -->
-        <form action="adicionar_conta.php" method="post">
-            <label for="nome">Nome:</label>
-            <input type="text" id="nome" name="nome" required><br><br>
+    <button id="addContaBtn">Adicionar Conta Bancária</button>
+    <button id="addReceitaBtn">Adicionar Nova Receita</button>
+    <button id="addDespesaBtn">Adicionar Nova Despesa</button>
 
-            <label for="tipo_conta">Tipo de Conta:</label>
-            <input type="text" id="tipo_conta" name="tipo_conta" required><br><br>
+    <!-- Modal para adicionar conta bancária -->
+    <div id="addContaModal" class="modal">
+        <div class="modal-content">
+            <span class="close" id="closeContaModal">&times;</span>
+            <h2>Adicionar Nova Conta Bancária</h2>
+            <form action="adicionar_conta.php" method="post">
+                <label for="nome">Nome:</label>
+                <input type="text" id="nome" name="nome" required><br><br>
 
-            <label for="saldo">Saldo:</label>
-            <input type="text" id="saldo" name="saldo" required><br><br>
+                <label for="tipo_conta">Tipo de Conta:</label>
+                <input type="text" id="tipo_conta" name="tipo_conta" required><br><br>
 
-            <input type="submit" value="Adicionar Conta">
-        </form>
+                <label for="saldo">Saldo:</label>
+                <input type="text" id="saldo" name="saldo" required><br><br>
+
+                <input type="submit" value="Adicionar Conta">
+            </form>
+        </div>
     </div>
 
-    <br>
+    <!-- Modal para adicionar receita -->
+    <div id="addReceitaModal" class="modal">
+        <div class="modal-content">
+            <span class="close" id="closeReceitaModal">&times;</span>
+            <h2>Adicionar Nova Receita</h2>
+            <form action="processar_receita.php" method="post">
+                <label for="valor">Valor:</label>
+                <input type="text" id="valor" name="valor" required><br><br>
 
-    <div class="form-container">
-        <h2>Adicionar Nova Receita</h2>
-        <!-- Formulário para adicionar receita -->
-        <form action="processar_receita.php" method="post">
-            <label for="valor">Valor:</label>
-            <input type="text" id="valor" name="valor" required><br><br>
+                <label for="categoria">Categoria:</label>
+                <select id="categoria" name="categoria">
+                    <option value="Salário">Salário</option>
+                    <option value="Outra fonte de renda">Outra fonte de renda</option>
+                </select><br><br>
 
-            <label for="categoria">Categoria:</label>
-            <select id="categoria" name="categoria">
-                <option value="Salário">Salário</option>
-                <option value="Outra fonte de renda">Outra fonte de renda</option>
-            </select><br><br>
+                <label for="conta_destino">Conta de Destino:</label>
+                <select id="conta_destino" name="conta_destino" required>
+                    <?php
+                    require_once 'conexao.php';
+                    $usuario_id = $_SESSION['usuario_id'];
+                    $sql = "SELECT id, nome FROM contas_bancarias WHERE usuario_id = ?";
+                    $stmt = $conn->prepare($sql);
+                    $stmt->bind_param("i", $usuario_id);
+                    $stmt->execute();
+                    $result = $stmt->get_result();
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<option value='{$row['id']}'>{$row['nome']}</option>";
+                    }
+                    $stmt->close();
+                    ?>
+                </select><br><br>
 
-            <label for="conta_destino">Conta de Destino:</label>
-            <select id="conta_destino" name="conta_destino" required>
-                <!-- Aqui você deve popular as opções com as contas bancárias do usuário -->
-                <?php
-                require_once 'conexao.php';
-                $usuario_id = $_SESSION['usuario_id'];
-                $sql = "SELECT id, nome FROM contas_bancarias WHERE usuario_id = ?";
-                $stmt = $conn->prepare($sql);
-                $stmt->bind_param("i", $usuario_id);
-                $stmt->execute();
-                $result = $stmt->get_result();
-                while ($row = $result->fetch_assoc()) {
-                    echo "<option value='{$row['id']}'>{$row['nome']}</option>";
-                }
-                $stmt->close();
-                ?>
-            </select><br><br>
-
-            <input type="submit" value="Adicionar Receita">
-        </form>
+                <input type="submit" value="Adicionar Receita">
+            </form>
+        </div>
     </div>
 
-    <br>
+    <!-- Modal para adicionar despesa -->
+    <div id="addDespesaModal" class="modal">
+        <div class="modal-content">
+            <span class="close" id="closeDespesaModal">&times;</span>
+            <h2>Adicionar Nova Despesa</h2>
+            <form action="processar_despesa.php" method="post">
+                <label for="valor">Valor:</label>
+                <input type="text" id="valor" name="valor" required><br><br>
 
-    <div class="form-container">
-        <h2>Adicionar Nova Despesa</h2>
-        <form action="processar_despesa.php" method="post">
-            <label for="valor">Valor:</label>
-            <input type="text" id="valor" name="valor" required><br><br>
+                <label for="categoria">Categoria:</label>
+                <input type="text" id="categoria" name="categoria" required><br><br>
 
-            <label for="categoria">Nome:</label>
-            <input type="text" id="categoria" name="categoria" required><br><br>
+                <label for="conta_id">Conta de Origem:</label>
+                <select id="conta_id" name="conta_id" required>
+                    <?php
+                    require_once 'conexao.php';
+                    $usuario_id = $_SESSION['usuario_id'];
+                    $sql = "SELECT id, nome FROM contas_bancarias WHERE usuario_id = ?";
+                    $stmt = $conn->prepare($sql);
+                    $stmt->bind_param("i", $usuario_id);
+                    $stmt->execute();
+                    $result = $stmt->get_result();
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<option value='{$row['id']}'>{$row['nome']}</option>";
+                    }
+                    $stmt->close();
+                    ?>
+                </select><br><br>
 
-            <label for="conta_id">Conta de Origem:</label>
-            <select id="conta_id" name="conta_id" required>
-                <?php
-                // Popula as opções com as contas bancárias do usuário
-                require_once 'conexao.php';
-                $usuario_id = $_SESSION['usuario_id'];
-                $sql = "SELECT id, nome FROM contas_bancarias WHERE usuario_id = ?";
-                $stmt = $conn->prepare($sql);
-                $stmt->bind_param("i", $usuario_id);
-                $stmt->execute();
-                $result = $stmt->get_result();
-                while ($row = $result->fetch_assoc()) {
-                    echo "<option value='{$row['id']}'>{$row['nome']}</option>";
-                }
-                $stmt->close();
-                ?>
-            </select><br><br>
-
-            <input type="submit" value="Adicionar Despesa">
-        </form>
+                <input type="submit" value="Adicionar Despesa">
+            </form>
+        </div>
     </div>
-
-    <br>
 
     <div class="contas">
-        <h2>Contas Bancárias</h2>
-        <!-- Exibição das contas bancárias do usuário -->
+        <h2 class="bv-home">Contas Bancárias</h2>
         <?php
         require_once 'conexao.php';
         $usuario_id = $_SESSION['usuario_id'];
@@ -140,13 +186,68 @@
                 echo "</div>";
             }
         } else {
-            // Caso não haja contas bancárias
             echo "<p>Nenhuma conta bancária encontrada.</p>";
         }
         $stmt->close();
         $conn->close();
         ?>
     </div>
+
+    <script>
+        // Obtém os elementos dos modais
+        var addContaModal = document.getElementById("addContaModal");
+        var addReceitaModal = document.getElementById("addReceitaModal");
+        var addDespesaModal = document.getElementById("addDespesaModal");
+
+        // Obtém os botões que abrem os modais
+        var addContaBtn = document.getElementById("addContaBtn");
+        var addReceitaBtn = document.getElementById("addReceitaBtn");
+        var addDespesaBtn = document.getElementById("addDespesaBtn");
+
+        // Obtém os elementos de fechar os modais
+        var closeContaModal = document.getElementById("closeContaModal");
+        var closeReceitaModal = document.getElementById("closeReceitaModal");
+        var closeDespesaModal = document.getElementById("closeDespesaModal");
+
+        // Adiciona eventos para abrir os modais
+        addContaBtn.onclick = function() {
+            addContaModal.style.display = "block";
+        }
+
+        addReceitaBtn.onclick = function() {
+            addReceitaModal.style.display = "block";
+        }
+
+        addDespesaBtn.onclick = function() {
+            addDespesaModal.style.display = "block";
+        }
+
+        // Adiciona eventos para fechar os modais
+        closeContaModal.onclick = function() {
+            addContaModal.style.display = "none";
+        }
+
+        closeReceitaModal.onclick = function() {
+            addReceitaModal.style.display = "none";
+        }
+
+        closeDespesaModal.onclick = function() {
+            addDespesaModal.style.display = "none";
+        }
+
+        // Fecha os modais ao clicar fora do conteúdo do modal
+        window.onclick = function(event) {
+            if (event.target == addContaModal) {
+                addContaModal.style.display = "none";
+            }
+            if (event.target == addReceitaModal) {
+                addReceitaModal.style.display = "none";
+            }
+            if (event.target == addDespesaModal) {
+                addDespesaModal.style.display = "none";
+            }
+        }
+    </script>
 
 </body>
 
