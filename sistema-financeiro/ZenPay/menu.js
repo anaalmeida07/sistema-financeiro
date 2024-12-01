85ef6a382271a8fcdbdaa12da91e192fc092bfb3
Luiz H. Menezes <79934587+LuizHenriqueMenezes@users.noreply.github.com>
2024-12-01 15:41:59 -0300

    Revert "up"
    This reverts commit 1734be431b4dd214fb2130c899f68f282f562521.

--- sistema-financeiro/ZenPay/menu.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    const menu = document.querySelector('.icons-home');
-    menu.classList.toggle('active');
-}
-
-function openCalculator() {
-    alert('Abrindo calculadora...');
-    // Implementação da calculadora vai aqui.
-}
